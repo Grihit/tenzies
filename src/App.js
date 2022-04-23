8d0d083929f6d746c3ca96d61b355451b3f302d0
Grihit <grihitbudhiraja12@gmail.com>
2022-04-23 20:33:20 +0530

    Added die

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react'
 import './App.css';
+import Die from './components/Die'
 
 function App() {
+  
+  const [dies, setDie] = React.useState(['1','1','1','1','1','1','1','1','1','1'])
+  const dieElements = dies.map(die => {
+    return <Die value={die} />
+  })
+  
   return (
     <main>
-
+      <div className='die--container'>
+        {dieElements}
+      </div>
     </main>
   );
 }
