9d12fd39023066a6792da26b0ec7d0c163055252
Grihit <grihitbudhiraja12@gmail.com>
2022-04-24 01:48:54 +0530

    Added random dice, roll button, fix die functions

--- src/App.js
+++ src/App.js
@@ -4,9 +4,44 @@ import Die from './components/Die'
 
 function App() {
   
-  const [dies, setDie] = React.useState(['1','1','1','1','1','1','1','1','1','1'])
+  const [dies, setDies] = React.useState([{}])
+  React.useEffect(() => {
+    const newDies = [{}]
+    for(let i=0;i<10;++i){
+      const newDie =
+      {
+          id: i+1, 
+          value: Math.floor(Math.random()*5 + 1), 
+          fixed: false
+      }
+      newDies[i]=newDie
+    }
+    setDies(newDies)
+  }, [])
+  
+  function changeDie(){
+    setDies(prevDies => {
+      return prevDies.map(die => {
+        return !die.fixed ? {...die, value: Math.floor(Math.random()*5 + 1)} : die
+      })
+    })
+  }
+
+  function fixDie(dieId){
+    setDies(prevDies => {
+      return prevDies.map(die => {
+        return die.id === dieId ? {...die, fixed: !die.fixed} : die
+      })
+    })
+  }
+
   const dieElements = dies.map(die => {
-    return <Die value={die} />
+    return <Die
+              class = {die.fixed? 'die--fix' : ''}
+              key={die.id} 
+              value={die.value}
+              click={() => fixDie(die.id)} 
+            />
   })
   
   return (
@@ -14,6 +49,7 @@ function App() {
       <div className='die--container'>
         {dieElements}
       </div>
+      <button className='roll' onClick={changeDie}>Roll</button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Die(props){
     return(
-        <div className='die'>
-            <span className='die--text'>{props.value}</span>
+        <div 
+            className={`die ${props.class}`} 
+            onClick={props.click}
+        >
+            <span className="die--text">{props.value}</span>
         </div>
     )
 }
